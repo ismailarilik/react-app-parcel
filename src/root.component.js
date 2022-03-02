ef624a5cdc52ae29d9f132141f48185eb696a64b
ismail.arilik <ismail.arilik@piworks.net>
2022-03-02 15:40:13 +0300

    Get library versions from apps
    It is a good example of cross-app import feature of single-spa, which is a way to share apps' data

--- src/root.component.js
+++ src/root.component.js
@@ -1,8 +1,11 @@
+import { getAngularVersion } from "@net-piworks/angular-app-parcel";
+import { getAngularjsVersion } from "@net-piworks/angularjs-app";
+
 export default function Root(props) {
   return (
     <section>
-      <a href="angular2">Angular app</a> |{" "}
-      <a href="angularjs">Angular.js app</a>
+      <a href="angular2">Angular app {getAngularVersion()}</a> |{" "}
+      <a href="angularjs">Angular.js app {getAngularjsVersion()}</a>
     </section>
   );
 }
